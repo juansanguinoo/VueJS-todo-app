094bd23b26722a09eec4d2b3ffd6ca56249023d5
Juan David Sanguino <edsanol07@outlook.com>
2023-02-14 12:14:19 -0500

    Todo app created with VueJS

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="todo" v-bind:class="{ completed: localCompleted }">
+    <div class="todo-body">
+      <input
+        type="checkbox"
+        v-bind:checked="localCompleted ? 'checked' : ''"
+        v-on:change="checkTodo"
+      />
+      {{ todo.title }}
+    </div>
+
+    <div class="todo-actions">
+      <button v-on:click="$emit('delete-todo', todo.id)">Eliminar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  data() {
+    return {
+      localCompleted: this.todo.completed,
+    };
+  },
+  methods: {
+    checkTodo() {
+      this.localCompleted = !this.localCompleted;
+    },
+  },
+  watch: {
+    localCompleted(newValue) {
+      this.$emit("update-todo", {
+        id: this.todo.id,
+        completed: newValue,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo {
+  border-bottom: solid 1px #ccc;
+  padding: 10px;
+}
+.todo:not(.completed):hover {
+  background-color: #eee;
+}
+.completed {
+  color: #ccc;
+  text-decoration: line-through;
+}
+.completed .todo-body {
+  text-decoration: line-through;
+}
+.todo-body,
+.todo-actions {
+  display: inline-block;
+  vertical-align: top;
+}
+.todo-body {
+  width: 85%;
+}
+.todo-actions {
+  padding: 0 10px;
+  width: 10%;
+}
+button {
+  border: none;
+  border-radius: 3px;
+  padding: 10px;
+  background-color: #ccc;
+  color: black;
+}
+button:hover {
+  background-color: #da2020;
+  color: white;
+}
+</style>
